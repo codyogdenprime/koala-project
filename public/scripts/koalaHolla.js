5dacfdcd25cc4b06c27f73daa3ae1616f8ab597c
Cody Ogden <cody@codyogden.com>
2016-09-15 11:29:10 -0500

    My Solve

--- public/scripts/koalaHolla.js
+++ public/scripts/koalaHolla.js
@@ -1,9 +1,92 @@
 console.log( 'js' );
 
+var transferSelect = function( transfer_status ) {
+
+  var select = $("<select />");
+  
+console.log( transfer_status );
+
+  var falseOpt = $("<option>false</option>");
+  var trueOpt = $("<option>true</option>");
+
+  if( transfer_status === "true" ) {
+    trueOpt.attr("selected", "true");
+  } else {
+    falseOpt.attr("selected", "true");
+  }
+
+  console.log( "opt true", trueOpt );
+
+  select.append( trueOpt );
+  select.append( falseOpt );
+
+  console.log( select );
+
+  return select;
+
+};
+
+var changeit = function() {
+
+  var kId = $( this ).data("koala-id");
+
+  var koala = {
+    id: kId,
+    name: $("#koala-" + kId + "-name").val(),
+    age: $("#koala-" + kId + "-age").val(),
+    sex: $("#koala-" + kId + "-sex").val(),
+    readyForTransfer: $("#koala-" + kId + "-transferStatus").val(),
+    notes: $("#koala-" + kId + "-notes").val()
+  };
+
+  editKoala( koala );
+
+};
+
+var displayKoalas = function( data ) {
+
+  var container = $("<div />", { class: 'container-koalas' });
+
+  for( var i = 0; i < data.length; i++ ) {
+
+    var wrapper = $("<div />", {class:'koala-wrapper'}).data("id", data[i].id );
+
+    var name = $('<input type="text" />', {class:'koala-name'} ).val( data[i].name );
+        name.on('change', changeit);
+        name.attr("id","koala-" + data[i].id + "-name");
+        name.data('koala-id', data[i].id );
+    var age = $('<input type="text" />', {class:'koala-age'}).val( data[i].age );
+        age.on('change', changeit);
+        age.attr("id","koala-" + data[i].id + "-age");
+        age.data('koala-id', data[i].id );
+    var sex = $('<input type="text" />', {class:'koala-sex'}).val( data[i].sex );
+        sex.on('change', changeit);
+        sex.data('koala-id', data[i].id );
+        sex.attr("id","koala-" + data[i].id + "-sex");
+    var transfer_status = transferSelect( data[i].transfer_status );
+        transfer_status.on('change', changeit);
+        transfer_status.data('koala-id', data[i].id );
+        transfer_status.attr("id","koala-" + data[i].id + "-transferStatus");
+    var notes = $('<input type="text" />', {class:'koala-notes'}).val( data[i].notes );
+        notes.on('change', changeit);
+        notes.attr("id","koala-" + data[i].id + "-notes");
+        notes.data('koala-id', data[i].id );
+
+    wrapper.append( name ).append( age ).append( sex ).append( transfer_status ).append( notes );
+
+    container.append( wrapper );
+
+  }
+
+  $("#koala-output").empty().append( container );
+
+};
+
 $( document ).ready( function(){
   console.log( 'JQ' );
   // load existing koalas on page load
   getKoalas();
+
 
   // add koala button click
   $( '#addButton' ).on( 'click', function(){
@@ -12,11 +95,28 @@ $( document ).ready( function(){
     // NOT WORKING YET :(
     // using a test object
     var objectToSend = {
-      name: 'testName',
-      age: 'testName',
-      sex: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
+      name: $('#nameIn').val(),
+      age: $('#ageIn').val(),
+      sex: $('#sexIn').val(),
+      readyForTransfer: $('#transferIn').val(),
+      notes: $('#notesIn').val(),
+    };
+    // call saveKoala with the new obejct
+    saveKoala( objectToSend );
+  }); //end addButton on click
+
+  $( '#editButton' ).on( 'click', function(){
+    console.log( 'in addButton on click' );
+    // get user input and put in an object
+    // NOT WORKING YET :(
+    // using a test object
+    var objectToSend = {
+      id: $('#idEditIn').val(),
+      name: $('#nameEditIn').val(),
+      age: $('#ageEditIn').val(),
+      sex: $('#sexEditIn').val(),
+      readyForTransfer: $('#transferEditIn').val(),
+      notes: $('#notesEditIn').val(),
     };
     // call saveKoala with the new obejct
     saveKoala( objectToSend );
@@ -31,10 +131,12 @@ var getKoalas = function(){
     type: 'GET',
     success: function( data ){
       console.log( 'got some koalas: ', data );
+      displayKoalas( data );
     } // end success
   }); //end ajax
   // display on DOM with buttons that allow edit of each
-} // end getKoalas
+
+}; // end getKoalas
 
 var saveKoala = function( newKoala ){
   console.log( 'in saveKoala', newKoala );
@@ -45,6 +147,21 @@ var saveKoala = function( newKoala ){
     data: newKoala,
     success: function( data ){
       console.log( 'got some koalas: ', data );
+      getKoalas();
     } // end success
   }); //end ajax
-}
+};
+
+var editKoala = function( newKoala ){
+  console.log( 'in saveKoala', newKoala );
+  // ajax call to server to get koalas
+  $.ajax({
+    url: '/editKoala',
+    type: 'put',
+    data: newKoala,
+    success: function( data ){
+      console.log( 'got some koalas: ', data );
+      getKoalas();
+    } // end success
+  }); //end ajax
+};
--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
+/* jshint esversion:6 */
 var express = require( 'express' );
 var app = express();
 var path = require( 'path' );
 var bodyParser= require( 'body-parser' );
 var urlencodedParser = bodyParser.urlencoded( {extended: false } );
 var port = process.env.PORT || 8080;
+
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/table_project';
+
 // static folder
 app.use( express.static( 'public' ) );
 
@@ -22,31 +27,103 @@ app.get( '/', function( req, res ){
 app.get( '/getKoalas', function( req, res ){
   console.log( 'getKoalas route hit' );
   //assemble object to send
-  var objectToSend={
-    response: 'from getKoalas route'
-  }; //end objectToSend
-  //send info back to client
-  res.send( objectToSend );
+  var objectToSend= {}; //end objectToSend
+  pg.connect( connectionString, ( err, client, done ) => {
+
+    if( err ) res.status(500).send( "Oops!");
+
+    var resultsArray = [];
+    var query = client.query( 'SELECT * FROM koalas ORDER BY id ASC' );
+
+    query.on( 'row', ( row ) => {
+      resultsArray.push( row );
+    });
+
+    query.on( 'end', ( ) => {
+      done();
+      return res.send( resultsArray );
+    });
+  });
+
 });
 
 // add koala
 app.post( '/addKoala', urlencodedParser, function( req, res ){
   console.log( 'addKoala route hit' );
-  //assemble object to send
-  var objectToSend={
-    response: 'from addKoala route'
-  }; //end objectToSend
-  //send info back to client
-  res.send( objectToSend );
+
+  var data = req.body;
+  var results = [];
+
+  console.log( "Data", data );
+
+  // Connect to database
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+
+        // SQL Query > Insert Data
+        client.query("INSERT INTO koalas(name, sex, age, transfer_status, notes) values($1, $2, $3, $4, $5)", [ data.name, data.sex, data.age, data.readyForTransfer, data.notes ]);
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM koalas ORDER BY id ASC");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.send(results);
+        });
+
+
+    });
 });
 
-// add koala
-app.post( '/editKoala', urlencodedParser, function( req, res ){
+// edit koala
+app.put( '/editKoala', urlencodedParser, function( req, res ){
   console.log( 'editKoala route hit' );
-  //assemble object to send
-  var objectToSend={
-    response: 'from editKoala route'
-  }; //end objectToSend
-  //send info back to client
-  res.send( objectToSend );
+
+
+  var data = req.body;
+  var results = [];
+
+  console.log( "Data", data );
+
+  // Connect to database
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+
+        // SQL Query > Insert Data
+        client.query("UPDATE koalas SET name=$2, sex=$3, age=$4, transfer_status=$5, notes=$6 WHERE id=$1", [ data.id, data.name, data.sex, data.age, data.readyForTransfer, data.notes ] );
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM koalas ORDER BY id ASC");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.send(results);
+        });
+
+
+    });
+
+
 });
